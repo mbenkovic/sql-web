c5dfad343bb7dbb3ab77d2a8ff8075fcd18babd5
mbenkovic <mario.benk@gmail.com>
2019-02-24 16:09:12 +0100

    Added first section.

--- script.js
+++ script.js
@@ -18,4 +18,17 @@ closeNavEl.addEventListener("click", closeNav);
 let openNavEl=document.getElementById("open-nav");
 openNavEl.addEventListener("click", openNav);
 
-// closeNav();
+/* Toggle Section */
+let toggleCreateEl=document.getElementById("toggle-create");
+toggleCreateEl.addEventListener("click", function(ev) {
+	if (ev) ev.preventDefault();
+	let sectionContentEl=document.querySelector("#section-create .section-content");
+	console.log(sectionContentEl);
+	if (sectionContentEl.classList.contains("hide")){
+		sectionContentEl.classList.remove("hide");
+		sectionContentEl.classList.add("show");
+	} else if (sectionContentEl.classList.contains("show")){
+		sectionContentEl.classList.remove("show");
+		sectionContentEl.classList.add("hide");
+	}
+});
