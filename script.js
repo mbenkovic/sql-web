1e7ef12be97c31abe1b252fb6031b8b854405886
mbenkovic <mario.benk@gmail.com>
2019-02-25 18:49:20 +0100

    Added open and close buttons for navigation; Added all sections

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function openNav(ev) {
 	console.log("openNav");
 	if (ev) ev.preventDefault();
 	document.getElementById("main-nav").style.display = "block";
+	document.getElementById("section-create").style.display = "none";
+	document.getElementById("section-insert").style.display = "none";
+	document.getElementById("section-update").style.display = "none";
+	document.getElementById("section-delete").style.display = "none";
 }
 
 /* Close */
@@ -10,6 +14,10 @@ function closeNav(ev) {
 	console.log("closeNav");
 	if (ev) ev.preventDefault();
 	document.getElementById("main-nav").style.display = "none";
+	document.getElementById("section-create").style.display = "block";
+	document.getElementById("section-insert").style.display = "block";
+	document.getElementById("section-update").style.display = "block";
+	document.getElementById("section-delete").style.display = "block";
 }
 
 let closeNavEl=document.getElementById("close-nav");
@@ -23,6 +31,48 @@ let toggleCreateEl=document.getElementById("toggle-create");
 toggleCreateEl.addEventListener("click", function(ev) {
 	if (ev) ev.preventDefault();
 	let sectionContentEl=document.querySelector("#section-create .section-content");
+	console.log(sectionContentEl);
+	if (sectionContentEl.classList.contains("hide")){
+		sectionContentEl.classList.remove("hide");
+		sectionContentEl.classList.add("show");
+	} else if (sectionContentEl.classList.contains("show")){
+		sectionContentEl.classList.remove("show");
+		sectionContentEl.classList.add("hide");
+	}
+});
+
+let toggleInsertEl=document.getElementById("toggle-insert");
+toggleInsertEl.addEventListener("click", function(ev) {
+	if (ev) ev.preventDefault();
+	let sectionContentEl=document.querySelector("#section-insert .section-content");
+	console.log(sectionContentEl);
+	if (sectionContentEl.classList.contains("hide")){
+		sectionContentEl.classList.remove("hide");
+		sectionContentEl.classList.add("show");
+	} else if (sectionContentEl.classList.contains("show")){
+		sectionContentEl.classList.remove("show");
+		sectionContentEl.classList.add("hide");
+	}
+});
+
+let toggleUpdateEl=document.getElementById("toggle-update");
+toggleUpdateEl.addEventListener("click", function(ev) {
+	if (ev) ev.preventDefault();
+	let sectionContentEl=document.querySelector("#section-update .section-content");
+	console.log(sectionContentEl);
+	if (sectionContentEl.classList.contains("hide")){
+		sectionContentEl.classList.remove("hide");
+		sectionContentEl.classList.add("show");
+	} else if (sectionContentEl.classList.contains("show")){
+		sectionContentEl.classList.remove("show");
+		sectionContentEl.classList.add("hide");
+	}
+});
+
+let toggleDeleteEl=document.getElementById("toggle-delete");
+toggleDeleteEl.addEventListener("click", function(ev) {
+	if (ev) ev.preventDefault();
+	let sectionContentEl=document.querySelector("#section-delete .section-content");
 	console.log(sectionContentEl);
 	if (sectionContentEl.classList.contains("hide")){
 		sectionContentEl.classList.remove("hide");
